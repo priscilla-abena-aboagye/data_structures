01dd4c79be8b9acafdd7c3e7d466dac9ade26a93
Abena <priscillaabenaaboagye@gmail.com>
2025-02-13 12:51:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 first_name = "priscilla"
 print(type(first_name))
 my_short_text = '''
@@ -7,3 +9,7 @@ I listen to alan walker.
 I have two brothers.
 '''
 print(my_short_text)
+my_school = "soronko academy"
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
