ed7ece433fb124b19f9fe30b3452b359e24914d7
Abena <priscillaabenaaboagye@gmail.com>
2025-04-21 07:46:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,5 @@ import string
 
 first_name = "priscilla"
 print(type(first_name))
-my_short_text = '''
-my name is Priscilla.
-I am a girl. 
-I listen to alan walker.
-I have two brothers.
-'''
-print(my_short_text)
-my_school = "soronko academy"
+my_school = "Good Hope School"
 print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.punctuation)
-print(string.digits)
-print(repr(string.whitespace))
