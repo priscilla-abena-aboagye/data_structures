54fcf9fa2d4fc8063db15be87ef0a6cd19bc3448
Abena <priscillaabenaaboagye@gmail.com>
2025-04-21 08:16:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print(type(first_name))
 my_school = "Good Hope School"
 print(string.ascii_letters)
 print(repr(string.whitespace))
+my_grades = (10, 9, 8, 10, 10)
+print(len(my_grades))
+for i in my_grades:
+    print(i)
+
