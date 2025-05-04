09514bd086879f2cbaa51a4fdfd2d7a5a99b89d1
Abena <priscillaabenaaboagye@gmail.com>
2025-05-04 13:31:23 +0000

    Question 7 and 8

--- loop.py
+++ loop.py
@@ -1,3 +1,4 @@
+'''
 # Question 1
 def prints_even_numbers():
     numbers = 0
@@ -7,6 +8,7 @@ def prints_even_numbers():
            print("Even numbers: ", num)
        numbers += 1
 prints_even_numbers()
+print("----------------------------")
 
 # Question 2
 def sum_of_numbers():
@@ -16,6 +18,7 @@ def sum_of_numbers():
         print(total_numbers)
     total_numbers += 1 
 sum_of_numbers()
+print("--------------------------------")
 
 # Question 3
 def prints_character(E):
@@ -23,6 +26,7 @@ def prints_character(E):
         print(i)
 
 prints_character("SwiftWay") 
+print("-------------------------------")
 
 # Question 4
 fruits = [
@@ -30,11 +34,31 @@ fruits = [
 ]
 for fruit in fruits:
     print(fruit.upper())
+print("-------------------------")
 
 # Question 5
 numbers = range(1, 11)
 for number in numbers:
     print(pow(number, 2))
+print("------------------------")
 
 # Question 6
+backwards = list(range(11))
+counting_backwards = backwards[::-1]
+for back in counting_backwards:
+    print(back)
+print("------------------------")
+'''
 
+# Question 7
+list_of_numbers= [3, 7, 9, 2, 5]
+for num in list_of_numbers:
+    if num > 5:
+        print(num)
+print("---------------------------")
+
+# Question 8
+mul_of_num = range(1, 13)
+for num in mul_of_num:
+    if num % 3 == 0:
+        print(num)
